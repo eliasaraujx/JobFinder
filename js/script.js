1641d96c9d0fa5541e18afd0a0f5a8e2ad77dabd
Elias Araujo <eliasaraujxdev@gmail.com>
2023-05-22 23:36:55 -0300

    Barra de navegação responsiva
    1.1 Barra de navegação responsiva.
    1.2 Adicionado função abre e fecha no menu para mobile.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-console.log('funcionou');
+$( document ).ready(function(){
+
+    var btnMenuMobile = $('.btn-menumobile');
+
+    $(btnMenuMobile).on('click', function(){
+
+        $('.nav-container ul').toggleClass('open');
+
+    })
+
+});
